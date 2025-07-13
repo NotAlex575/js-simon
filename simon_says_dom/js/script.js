fcb903e7f930676bdf8b62ff196a3fdabc793e03
NotAlex575 <dapontealessandro89gmail.com>
2025-07-13 12:12:58 +0200

    generato i 5 numeri da indovinare, con la loro rimozione nella pagina html dopo 5 secondi

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,9 +1,19 @@
 const countdown = document.getElementById("countdown");
 const instructions_before = document.getElementById("instructions-before");
 const instructions_after = document.getElementById("instructions-after");
+const numbers_list = document.getElementById("numbers-list");
 
 
 let timer = 5;
+let random_number = [];
+
+//iniziamo a creare 5 numeri randomici da indovinare
+for(let i = 0; i < 5; i++){
+    const li = document.createElement("li");
+    random_number[i] = Math.floor(Math.random()*100)+1
+    li.innerHTML = random_number[i];
+    numbers_list.appendChild(li);
+}
 
 function timer_function(){
     if(timer > 0){
@@ -13,9 +23,10 @@ function timer_function(){
         setTimeout(timer_function, 1000)
     }
     else{
-        //togliamo la visibilità a cooldown e instructions-before
+        //togliamo la visibilità a cooldown instructions-before e togliamo i valori dentro a numbers_list
         countdown.classList.add("d-none");
         instructions_before.classList.add("d-none");
+        numbers_list.innerHTML= "";
         //aggiungiamo la visibilità a instructions-after
         instructions_after.classList.remove("d-none");
 
