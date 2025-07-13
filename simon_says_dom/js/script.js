3eca7382b6347b290b004dc7ec52ee9ecc3d6cd0
NotAlex575 <dapontealessandro89gmail.com>
2025-07-13 12:41:36 +0200

    aggiunto inserimento value numeri per indovinare i 5 numeri generati randomicamente

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -2,6 +2,11 @@ const countdown = document.getElementById("countdown");
 const instructions_before = document.getElementById("instructions-before");
 const instructions_after = document.getElementById("instructions-after");
 const numbers_list = document.getElementById("numbers-list");
+const answers_form = document.getElementById("answers-form");
+
+const form = document.getElementById("answers-form");
+const inputs = form.querySelectorAll("input");
+const message = document.getElementById("message")
 
 
 let timer = 5;
@@ -10,11 +15,12 @@ let random_number = [];
 //iniziamo a creare 5 numeri randomici da indovinare
 for(let i = 0; i < 5; i++){
     const li = document.createElement("li");
-    random_number[i] = Math.floor(Math.random()*100)+1
+    random_number[i] = Math.floor(Math.random()*50)+1
     li.innerHTML = random_number[i];
     numbers_list.appendChild(li);
 }
 
+//TIMER CHE MOSTRA I 5 NUMERI, E DOPO CHE IL TIMER E A 0; RIMUOVE I 5 NUMERI
 function timer_function(){
     if(timer > 0){
         console.log(timer)
@@ -29,9 +35,27 @@ function timer_function(){
         numbers_list.innerHTML= "";
         //aggiungiamo la visibilità a instructions-after
         instructions_after.classList.remove("d-none");
-
+        answers_form.classList.remove("d-none")
     }
 }
+
+form.addEventListener("submit", function (event_submit) {
+    let numeri_corretti = 0;
+    event_submit.preventDefault();
+    users_numbers = [];
+    for (let i = 0; i < inputs.length; i++) {
+        users_numbers.push(parseInt(inputs[i].value));
+    }
+    for (let i = 0; i < inputs.length; i++){
+        for(let j = 0; j < inputs.length; j++){
+            if(random_number[i] == users_numbers[j]){
+                numeri_corretti++;
+            }   
+        }
+    }
+    console.log(numeri_corretti)
+    message.innerHTML = "Hai trovato "+numeri_corretti+" numeri!"
+})
 
 
 
